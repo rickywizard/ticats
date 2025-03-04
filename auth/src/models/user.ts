daf2a91c55bc8258a5688c8443035e853e08d0e7
rickywizard <rickywijayatatando@gmail.com>
2025-03-04 15:33:58 +0700

    feat: auth service

---
+++ auth/src/models/user.ts
@@ -0,0 +1,56 @@
+import mongoose, { mongo } from 'mongoose';
+import { PasswordManager } from '../services/password-manager';
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongo.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('email')) {
+    const hashed = await PasswordManager.toHash(this.get('password') as string);
+    this.set('password', hashed);
+  }
+
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  if (mongoose.connection.db) {
+    const colllections = await mongoose.connection.db.collections();
+
+    for (let collection of colllections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
