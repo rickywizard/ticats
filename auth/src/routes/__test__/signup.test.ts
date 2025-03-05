52d55870ff4e6a6d2d97785a47639197f3e016f4
rickywizard <rickywijayatatando@gmail.com>
2025-03-05 17:04:46 +0700

    test: signup test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,4 +1,66 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('returns a 201 on successful signup', async () => {});
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'aasdfsdaf',
+      password: 'sdfafdadsf',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '12',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: '123456788',
+    })
+    .expect(400);
+});
+
+it('disallows duplicate email', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '123456789',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '123456789',
+    })
+    .expect(400);
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,10 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
 
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdfasdf';
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
