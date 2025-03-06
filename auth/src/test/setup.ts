39d58ab5875e4cb31c28e1e33a683e99ee5bed4b
rickywizard <rickywijayatatando@gmail.com>
2025-03-06 11:22:41 +0700

    test: add get cookie sign up helper

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+  var getSignupCookie: () => Promise<string[]>;
+}
 
 let mongo: any;
 
@@ -27,3 +33,24 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.getSignupCookie = async () => {
+  const email = 'test@test.com';
+  const password = 'asdfasdf';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  if (!cookie) {
+    throw new Error('Faile to get cookie from response');
+  }
+
+  return cookie;
+};
